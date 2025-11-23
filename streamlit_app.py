58b1db7cc1db82fede21caf9fc1ef2055ed6061e
cnagamon99 <152366406+cnagamon99@users.noreply.github.com>
2025-11-23 06:58:59 -0800

    Initialize Streamlit app with basic layout

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+
+st.set_page_config(page_title="Streamlit LKangchain Demo", page_icon=":robot:")
+st.header("Streamlist Langchain Demo")
+
+st.subheader("Answer: ")
+st.write("Hello")
